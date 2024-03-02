b8ca7150d55b4ece1e13463e66bd2aa17bd051c7
MarcosLS <marcos27srs@gmail.com>
2024-03-01 23:30:21 -0300

    * CRIAÇÃO DO DISPLAY E FUNDO * INSERÇÃO DE MÚSICA NO MENU * CRIAÇÃO DO TEXTO NO MENU

---
+++ GameShooter/code/Game.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pygame as pygame
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Menu import Menu
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
