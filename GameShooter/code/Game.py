3fd1a7a648c5b615bf9347749a3552530c9e8328
MarcosLuis <marcos27srs@gmail.com>
2024-03-19 23:42:48 -0300

    ADICIONANDO COMENTÁRIO E LEGENDA PARA FACILITAR ENTENDER O FUNCIONAMENTO DO CÓDIGO.

--- GameShooter/code/Game.py
+++ GameShooter/code/Game.py
@@ -1,29 +1,60 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import sys
+import sys  # Importa o módulo 'sys' para manipulação do sistema
 
-import pygame as pygame
-from pygame import Surface, Rect
-from pygame.font import Font
+import pygame as pygame  # Importa a biblioteca Pygame com o alias 'pygame'
 
-from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
-from code.Level import Level
-from code.Menu import Menu
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION  # Importa constantes específicas do jogo
+from code.Level import Level  # Importa a classe Level do jogo
+from code.Menu import Menu  # Importa a classe Menu do jogo
 
 
 class Game:
     def __init__(self):
-        pygame.init()
-        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
+        pygame.init()  # Inicializa o pygame
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))  # Cria uma janela de exibição
 
     def run(self):
         while True:
-            menu = Menu(self.window)
-            menu_return = menu.run()
+            menu = Menu(self.window)  # Cria uma instância da classe Menu
+            menu_return = menu.run()  # Executa o menu e recebe o retorno
 
-            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
-                level = Level(self.window, 'Level1', menu_return)
-                level_return = level.run()
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:  # Verifica se a opção do menu é válida
+                level = Level(self.window, 'Level1', menu_return)  # Cria uma instância da classe Level
+                level_return = level.run()  # Executa o nível e recebe o retorno
             else:
-                pygame.quit()
-                sys.exit()
+                pygame.quit()  # Fecha o pygame
+                sys.exit()  # Encerra o programa
+
+# Importações de bibliotecas e módulos:
+# import sys: Importa o módulo sys, que fornece acesso a algumas variáveis usadas ou mantidas pelo interpretador e
+# interação com o ambiente Python.
+# import pygame: Importa a biblioteca Pygame, que é usada para criar jogos em Python.
+# from pygame import Surface, Rect: Importa as classes Surface e Rect do Pygame, que são usadas para representar
+# superfícies e retângulos, respectivamente.
+# from pygame.font import Font: Importa a classe Font do Pygame, que é usada para manipular fontes de texto.
+
+# Importações locais personalizadas:
+# from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION: Importa constantes específicas do jogo localizadas no
+# módulo Const.
+# from code.Level import Level: Importa a classe Level do jogo localizada no módulo Level.
+# from code.Menu import Menu: Importa a classe Menu do jogo localizada no módulo Menu.
+
+# Classe Game:
+# Define uma classe chamada Game.
+# No método __init__, inicializa o Pygame com pygame.init() e cria uma janela de exibição com o tamanho especificado
+# pelas constantes WIN_WIDTH e WIN_HEIGHT.
+# No método run, inicia um loop principal do jogo.
+# Dentro do loop:
+# Cria uma instância da classe Menu, que é responsável por exibir o menu do jogo.
+# Executa o menu e armazena o retorno.
+# Verifica se a escolha do menu é uma das opções válidas.
+# Se for uma opção válida, cria uma instância da classe Level, passando o nome do nível ('Level1') e a opção do menu
+# como parâmetros.
+# Executa o nível.
+# Se a escolha do menu não for válida, encerra o jogo usando pygame.quit() e sys.exit().
+
+# Uso das constantes e classes importadas:
+# As constantes WIN_WIDTH, WIN_HEIGHT e MENU_OPTION são utilizadas para configurar o tamanho da janela e as opções
+# do menu.
+# As classes Level e Menu são instanciadas e utilizadas dentro do loop principal para exibir o menu e os níveis do jogo.
--- GameShooter/code/Menu.py
+++ GameShooter/code/Menu.py
@@ -1,58 +1,88 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import sys
+import sys  # Importa o módulo 'sys' para manipulação do sistema
 
 import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, \
+    COLOR_YELLOW  # Importa constantes e cores do jogo
 
 
 class Menu:
     def __init__(self, window):
-        self.window: Surface = window
-        self.surf = pygame.image.load('./asset/MenuBG.png').convert_alpha()
-        self.rect = self.surf.get_rect(left=0, top=0)
+        self.window: Surface = window  # Define a janela de exibição para o menu
+        self.surf = pygame.image.load('./asset/MenuBG.png').convert_alpha()  # Carrega a imagem de fundo do menu
+        self.rect = self.surf.get_rect(left=0, top=0)  # Obtém o retângulo que envolve a imagem de fundo
 
     def run(self):
-        pygame.mixer_music.load('./asset/SoundMenuBG.mp3')
-        pygame.mixer_music.play(-1)
-        menu_option = 0
+        pygame.mixer_music.load('./asset/SoundMenuBG.mp3')  # Carrega a música de fundo do menu
+        pygame.mixer_music.play(-1)  # Reproduz a música de fundo em loop
+        menu_option = 0  # Inicializa a opção do menu selecionada como 0
         while True:
-            self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Shooter RPG", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(30, "Multiplayer", COLOR_ORANGE, ((WIN_WIDTH / 2), 110))
-            for i in range(len(MENU_OPTION)):
-                if i == menu_option:
-                    self.menu_text(18, MENU_OPTION[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 200 + 30 * i))
+            self.window.blit(source=self.surf, dest=self.rect)  # Desenha a imagem de fundo do menu na janela
+            self.menu_text(50, "Shooter RPG", COLOR_ORANGE,
+                           ((WIN_WIDTH / 2), 70))  # Adiciona texto "Shooter RPG" ao menu
+            self.menu_text(30, "Multiplayer", COLOR_ORANGE,
+                           ((WIN_WIDTH / 2), 110))  # Adiciona texto "Multiplayer" ao menu
+            for i in range(len(MENU_OPTION)):  # Itera sobre as opções do menu
+                if i == menu_option:  # Se esta é a opção do menu atualmente selecionada
+                    self.menu_text(18, MENU_OPTION[i], COLOR_YELLOW,
+                                   ((WIN_WIDTH / 2), 200 + 30 * i))  # Adiciona texto destacado para
+                    # esta opção
                 else:
-                    self.menu_text(18, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 30 * i))
-            pygame.display.flip()
+                    self.menu_text(18, MENU_OPTION[i], COLOR_WHITE,
+                                   ((WIN_WIDTH / 2), 200 + 30 * i))  # Adiciona texto normal para as
+                    # outras opções
+            pygame.display.flip()  # Atualiza a janela
 
-            # VERIFICAÇÃO DE EVENTOS:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_DOWN:
-                        if menu_option <= len(MENU_OPTION) - 1:
-                            menu_option += 1
+            # Verificação de eventos:
+            for event in pygame.event.get():  # Itera sobre os eventos na fila de eventos
+                if event.type == pygame.QUIT:  # Se o evento for o fechamento da janela
+                    pygame.quit()  # Encerra o Pygame
+                    sys.exit()  # Sai do programa
+                if event.type == pygame.KEYDOWN:  # Se uma tecla foi pressionada
+                    if event.key == pygame.K_DOWN:  # Se a tecla para baixo foi pressionada
+                        if menu_option <= len(MENU_OPTION) - 1:  # Se a opção do menu atual não é a última
+                            menu_option += 1  # Avança para a próxima opção do menu
                         else:
-                            menu_option = 0
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        if menu_option >= 0:
-                            menu_option -= 1
+                            menu_option = 0  # Volta para a primeira opção do menu
+                    if event.key == pygame.K_UP:  # Se a tecla para cima foi pressionada
+                        if menu_option >= 0:  # Se a opção do menu atual não é a primeira
+                            menu_option -= 1  # Retrocede para a opção do menu anterior
                         else:
-                            menu_option = len(MENU_OPTION) - 1
-                    if event.key == pygame.K_RETURN:
-                        return MENU_OPTION[menu_option]
+                            menu_option = len(MENU_OPTION) - 1  # Vai para a última opção do menu
+                    if event.key == pygame.K_RETURN:  # Se a tecla Enter foi pressionada
+                        return MENU_OPTION[menu_option]  # Retorna a opção do menu selecionada
 
-    # INSERÇÃO DE FONTE
+    # Função para adicionar texto ao menu
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
-        text_font: Font = pygame.font.SysFont("Lucida Sans Typewriter", size=text_size)
-        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
-        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
-        self.window.blit(source=text_surf, dest=text_rect)
+        text_font: Font = pygame.font.SysFont("Lucida Sans Typewriter", size=text_size)  # Define a fonte do texto
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()  # Renderiza o texto na
+        # superfície
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)  # Obtém o retângulo envolvendo o texto
+        self.window.blit(source=text_surf, dest=text_rect)  # Desenha o texto na janela
+
+# Importações de Bibliotecas e Módulos:
+# import sys: Importa o módulo 'sys', que é utilizado para interação com o sistema e encerramento do programa.
+# import pygame.image: Importa o módulo image da biblioteca Pygame, que é usado para carregar imagens.
+# From pygame import Surface, Rect: Importa as classes Surface e Rect da biblioteca Pygame, que são usadas para
+# manipulação de superfícies e retângulos, respectivamente.
+# From pygame.font import Font: Importa a classe Font da biblioteca Pygame, que é usada para renderizar texto na tela.
+
+# Importações Locais Personalizadas:
+# from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW: Importa constantes
+# específicas do jogo do módulo Const, como a largura da janela, cores e opções de menu.
+
+# Definição da Classe Menu:
+# A classe Menu é responsável por exibir e interagir com o menu do jogo.
+# No método __init__, a janela do menu é configurada, a imagem de fundo do menu é carregada e o retângulo que
+# envolve essa imagem é obtido.
+# O método run() controla a lógica do menu:
+# Carrega e reproduz a música de fundo do menu.
+# Exibe a imagem de fundo do menu e renderiza o texto das opções do menu.
+# Responde aos eventos do teclado, permitindo ao jogador navegar pelas opções do menu usando as teclas de seta
+# para cima e para baixo.
+# Quando o jogador pressiona Enter, a opção selecionada é retornada.
+# O método menu_text() é responsável por renderizar o texto na tela, utilizando a fonte e as cores especificadas.
